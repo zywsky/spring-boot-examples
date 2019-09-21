4278a9ef523b7c12551c7ce024617e77936785b2
zywsky <906514738@qq.com>
2019-09-21 15:43:21 +0800

    u

--- spring-boot-hello/src/main/java/com/neo/controller/HelloController.java
+++ spring-boot-hello/src/main/java/com/neo/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.neo.controller;
+age com.neo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,4 +10,5 @@ public class HelloController {
     public String index() {
         return "Hello Spring Boot 2.0!";
     }
+
 }
